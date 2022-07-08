b1df7c472a352913e8c39306e3caae18ea5e38a7
Sirspam <71392316+Sirspam@users.noreply.github.com>
2022-07-08 14:20:45 +0100

    Changed StaticFields get accessors
    I'm not completely certain that { get } caches the result from the get plugin query, so done this instead.
    
    Is the get plugin query even that intensive? probably not
    
    Do I know when it would be ideal to use auto properties? not really

--- MultiCode_inator/Utils/StaticFields.cs
+++ MultiCode_inator/Utils/StaticFields.cs
@@ -4,9 +4,26 @@ namespace MultiCode_inator.Utils
 {
     public static class StaticFields
     {
+        private static bool? _catCoreInstalled;
+        private static bool? _beatSaberPlusInstalled;
+        
         public static string? RoomCode;
-        public static bool CatCoreInstalled { get; } = PluginManager.GetPluginFromId("CatCore") != null;
-        public static bool BeatSaberPlusInstalled { get; } = PluginManager.GetPluginFromId("BeatSaberPlusCORE") != null;
+        public static bool CatCoreInstalled
+        {
+            get
+            {
+                _catCoreInstalled ??= PluginManager.GetPluginFromId("CatCore") != null;
+                return (bool) _catCoreInstalled;
+            }
+        }
+        public static bool BeatSaberPlusInstalled
+        {
+            get
+            {
+                _beatSaberPlusInstalled ??= PluginManager.GetPluginFromId("BeatSaberPlusCORE") != null;
+                return (bool) _beatSaberPlusInstalled;
+            }
+        }
         public static readonly bool DependencyInstalled = CatCoreInstalled || BeatSaberPlusInstalled;
         public const string NoDependenciesMessage = "Neither CatCore or BeatSaberPlus is installed. MultiCode-inator won't work without one of these mods installed!";
     }
